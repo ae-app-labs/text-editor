962f41cd065c2ee98be80bc0e938a0030beef19d
Midhun Harikumar <midhunhk@gmail.com>
2017-08-13 21:29:52 +0530

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,23 @@
+// Transparancy only works on Windows if WDM is enabled
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+app.on("ready", _ =>{
+  
+    let win = new BrowserWindow({
+      height: 400,
+      width: 350,
+		transparent: true,
+		frame: false,
+		toolbar: true
+    });
+
+    win.loadURL(`file://${__dirname}/editor.html`)
+
+    win.on("closed", _ =>{
+      mainWindow = null;
+    })
+
+});
